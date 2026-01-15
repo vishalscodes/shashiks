35105c07fd085bafcc7902c88e642d9824f0feeb
V <104351431+vishalscodes@users.noreply.github.com>
2026-01-15 14:17:21 +0530

    Update

--- astro.config.ts
+++ astro.config.ts
@@ -1,5 +1,6 @@
 import { rehypeHeadingIds } from '@astrojs/markdown-remark'
-import vercel from '@astrojs/vercel'
+import netlify from '@astrojs/netlify'
+// import vercel from '@astrojs/vercel'
 import AstroPureIntegration from 'astro-pure'
 import { defineConfig, fontProviders } from 'astro/config'
 import rehypeKatex from 'rehype-katex'
@@ -25,17 +26,17 @@ import config from './src/site.config.ts'
 // https://astro.build/config
 export default defineConfig({
   // [Basic]
-  site: 'https://astro-pure.js.org',
+  site: 'https://shashiks.netlify.app',
   // Deploy to a sub path
   // https://astro-pure.js.org/docs/setup/deployment#platform-with-base-path
-  // base: '/astro-pure/',
+  // base: '/shashiks/',
   trailingSlash: 'never',
   // root: './my-project-directory',
   server: { host: true },
 
   // [Adapter]
   // https://docs.astro.build/en/guides/deploy/
-  adapter: vercel(),
+  adapter: netlify(),
   output: 'server',
   // Local (standalone)
   // adapter: node({ mode: 'standalone' }),
